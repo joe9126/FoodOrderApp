ddca4a673e4cb43b92bcd00fbb4d1916cff54a7d
joe9126 <joeasewe@gmail.com>
2023-07-13 15:52:00 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { MatCommonModule } from '@angular/material/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
